d6e8981b9a8e4aa9f276b33c839309470e18921e
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

---
+++ language/english/lang_main.php
@@ -0,0 +1,67 @@
+<?php
+/*	Project:	EQdkp-Plus
+ *	Package:	EQdkp-Plus Language File
+ *	Link:		http://eqdkp-plus.eu
+ *
+ *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: plugins/siggenerator/language/english/lang_main.php
+//Source-Language: german
+
+$lang = array( 
+	"siggenerator" => 'Signature generator',
+	"siggenerator_short_desc" => 'Signature generator',
+	"siggenerator_long_desc" => 'Creates signatures with Character-Data and Point-information',
+	"sg_plugin_not_installed" => 'The signature generator-plugin is not installed.',
+	"sg_manage_signatures" => 'Signature Management',
+	"sg_manage_fonts" => 'Font Management',
+	"sg_manage_backgrounds" => 'Background Management',
+	"sg_add_signature" => 'Add Signature',
+	"sg_background" => 'Background',
+	"sg_font" => 'Font',
+	"sg_font_color" => 'Fontcolor',
+	"sg_font_border_color" => 'Color Fontborder',
+	"sg_font_border_size" => 'Width Fontborder',
+	"sg_position" => 'Position',
+	"sg_preset" => 'Value',
+	"sg_add_field" => 'Add field',
+	"sg_values" => 'Fields',
+	"sg_delete_field" => 'Delete field',
+	"sg_live_preview" => 'Live-preview',
+	"sg_show_label" => 'Show label',
+	"sg_picture_preset" => 'Charimage module',
+	"sg_signatur_link" => 'Charactersignatures',
+	"sg_select_char" => 'Select character',
+	"sg_bbcode" => 'BB-Code for boards',
+	"sg_htmlcode" => 'HTML-Code for websites',
+	"sg_direktlink" => 'Direct link',
+	"sg_add_font" => 'Add font',
+	"sg_folder" => 'Folder',
+	"sg_add_background" => 'Add background',
+	"sg_font_help" => 'You can upload your own fonts. The fonts must have the .ttf-format. You can download free fonts from Google Fonts.',
+	"sg_background_help" => 'You can upload your own background. The best resolution is 500px width and 100px height. Other resolutions are possible, but can cause problems.',
+	
+);
+
+?>
