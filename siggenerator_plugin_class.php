1053b0de3df031433f208f4165fecf4014a95b84
GodMod <GodMod@users.noreply.github.com>
2015-02-10 09:33:40 +0100

    Update siggenerator_plugin_class.php

--- siggenerator_plugin_class.php
+++ siggenerator_plugin_class.php
@@ -29,7 +29,7 @@ if (!defined('EQDKP_INC')){
   +--------------------------------------------------------------------------*/
 class siggenerator extends plugin_generic{
 
-	public $version		= '0.1.0';
+	public $version		= '0.1.1';
 	public $build		= '1';
 	public $copyright	= 'GodMod';
 	public $vstatus		= 'Beta';
